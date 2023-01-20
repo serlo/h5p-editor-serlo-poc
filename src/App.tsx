410a511e8d8fe6a2754818ff806bbd190c9cfff9
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-01-20 13:10:12 +0100

    make more robust

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 export default function App(props) {
+  let val = "";
+
+  try {
+    val = atob(props.value);
+  } catch (e) {}
+
   return (
     <div>
       <p>Das ist eine React Komponente:</p>
       <textarea
-        defaultValue={atob(props.value)}
+        defaultValue={val}
         onChange={(e) => {
           props.update(btoa(e.target.value));
         }}
